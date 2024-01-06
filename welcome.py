6686b3026d4e7f09355e13245a32c90febe45cd7
Prashant <acharyaprashant227@gmail.com>
2024-01-06 09:58:58 +0545

    Created and started Python

---
+++ welcome.py
@@ -0,0 +1,38 @@
+# print("This is python first program by me")
+# # List datatype
+# my_list=["Prashant","Acharya"]
+# print(my_list[0])
+
+# aboutMe=["Prashant",17,2062,True]
+# print(aboutMe)
+
+# num1=34
+# num2=434
+
+# Operator in python
+# print(num1 ** 3)
+# print(num1 - num2)
+# print(num2 / num1)
+# print(num2 // num1)
+# print(num2 % num1)
+
+# FInd the avg of the number
+
+# num1=5
+# print(type(num1))
+# num2=9
+# avg=int((num1+num2)/2)
+# print(avg)
+
+# logical operator in python
+
+x=True
+y=False
+
+print(x and y)  #false
+
+print(x or y) #true
+print(not x)  #false
+
+ 
+ 
