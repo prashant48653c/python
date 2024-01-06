6f6b83a772589c0e86392ecf281c3d6156c0a84b
Prashant <acharyaprashant227@gmail.com>
2024-01-06 11:54:32 +0545

    Learned more python and made vid downloader from pytube

--- welcome.py
+++ welcome.py
@@ -26,13 +26,97 @@
 
 # logical operator in python
 
-x=True
-y=False
+# x=True
+# y=False
 
-print(x and y)  #false
+# print(x and y)  #false
 
-print(x or y) #true
-print(not x)  #false
+# print(x or y) #true
+# print(not x)  #false
+
+# if else
+
+# age=18
+
+# if(age >18):
+#     print("You can vote")
+# elif(age == 18):
+#     print("YOu will vote from next year")
+#     print(list(range(18)))
+# else:
+#     print("YOu can't vote")
+
+# for x in range(11):
+#     print(x ** x ) 
+
+# arr=["ravi","sukra","nishan"]
+# for y in arr:
+#     print(y.upper())
+
+# age=0 
+# while(age<18):
+#     print(age)
+#     if(age == 4):
+#          break
+#     age +=1
+
+# Strings in python
+    
+# name='''     rH3EAdER '''
+# #       0123456   index
+# print(name.casefold())
+# print(name.capitalize())
+# print(name[2:5])
+# print(name.isalpha())
+# print(name.isdigit())
+# print(name.rstrip())
+ 
+# for x in name:
+#     print(x)
+
+#List in python (Array)
+# myList=["Prashant",17,3.64]
+# print(myList[-1])
+# del myList[1]
+# print(myList)
+
+# myEvenList=[x for x in range(101) if x %3 ==0]
+# print(myEvenList)
+
+
+# mySquareList=[x**2 for x in range(101) if x %3 ==0]
+# # print(mySquareList)
+# mySquareList.insert(0,44)
+# mySquareList.append(5)
+# mySquareList.sort()
+# mySquareList.pop()
+# print(mySquareList)
+
+# tup=(1,2,3,4)
+# print(tup)
+
+# seet={1,3,4,6,3,4,5}
+# print(seet)
+
+# obj={"name":"prashant","age":44}
+# print(obj["age"])
+
+# from math import *
+# print(pi)
+# print(cos(3.44))
+
+# def sayHello(name="Unknown"):
+#     for x in name:
+#         print("Hello", x)
 
  
- 
+# people=["Prashant","Vishru","Dinest"]
+# sayHello(people)
+
+# fileObj=open('data.txt',"r")
+# print(fileObj.read())
+# fileObj.close()
+
+# with open('write.txt','w') as f:
+#    print(f.writelines("THis is the write folder "))
+
---
+++ yt.py
@@ -0,0 +1,8 @@
+# from pytube import YouTube
+
+# url="https://www.youtube.com/watch?v=7BXJIjfJCsA"
+# myvideo=YouTube(url)
+# print(myvideo.title)
+# max_video=myvideo.streams.get_highest_resolution()
+# max_video.download()
+ 
